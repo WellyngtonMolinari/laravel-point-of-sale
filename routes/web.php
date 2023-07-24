51cade9a163282a45d3adfa9649f8d2b2d7ca268
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-07-24 13:12:32 -0300

    Added new Edit roles permission page

--- routes/web.php
+++ routes/web.php
@@ -232,6 +232,7 @@ Route::middleware(['auth'])->group(function()
     Route::post('/role/permission/store','StoreRolesPermission')->name('role.permission.store');
 
      Route::get('/all/roles/permission','AllRolesPermission')->name('all.roles.permission');
+     Route::get('/admin/edit/roles/{id}','AdminEditRoles')->name('admin.edit.roles');
    
    });
    
