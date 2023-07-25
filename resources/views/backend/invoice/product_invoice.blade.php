f6b3827931f2b29b90498f6bedc00f7e44033eac
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-07-25 08:02:17 -0300

    New Due amount system

--- resources/views/backend/invoice/product_invoice.blade.php
+++ resources/views/backend/invoice/product_invoice.blade.php
@@ -191,10 +191,7 @@
                 </div>
 
 
-                    <div class="mb-3">
-            <label for="username" class="form-label">Due Amount</label>
-    <input class="form-control" type="text" name="due" placeholder="Due Amount ">
-                </div>
+
 
     <input type="hidden" name="customer_id" value="{{ $customer->id }}">
    <input type="hidden" name="order_date" value="{{ date('d-F-Y') }}">
