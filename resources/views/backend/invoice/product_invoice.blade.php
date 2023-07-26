74c425d33a12eccb2aa35ea8496542871f7e3357
Wellyngton Molinari <wellymolinari97@gmail.com>
2023-07-26 17:01:16 -0300

    invoice traduzido

--- resources/views/backend/invoice/product_invoice.blade.php
+++ resources/views/backend/invoice/product_invoice.blade.php
@@ -13,10 +13,10 @@
                                 <div class="page-title-box">
                                     <div class="page-title-right">
                                         <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Customer Invoice</a></li>
+                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Cobrar Cliente</a></li>
                                         </ol>
                                     </div>
-                                    <h4 class="page-title">Customer Invoice</h4>
+                                    <h4 class="page-title">Gerar Cobrança</h4>
                                 </div>
                             </div>
                         </div>     
@@ -44,22 +44,22 @@
                                             </div>
                                         </div>
                                         <div class="float-end">
-                                            <h4 class="m-0 d-print-none">Invoice</h4>
+                                            <h4 class="m-0 d-print-none">Cobrança</h4>
                                         </div>
                                     </div>
 
             <div class="row">
                 <div class="col-md-6">
                     <div class="mt-3">
-                        <p><b>Hello, {{ $customer->name }}</b></p>
+                        <p><b>Olá, {{ $customer->name }}</b></p>
                         </div>
                     
                     </div><!-- end col -->
                     <div class="col-md-4 offset-md-2">
                         <div class="mt-3 float-end">
-                            <p><strong>Order Date : </strong> <span class="float-end"> &nbsp;&nbsp;&nbsp;&nbsp; Jan 17, 2016</span></p>
-                            <p><strong>Order Status : </strong> <span class="float-end"><span class="badge bg-danger">Unpaid</span></span></p>
-                            <p><strong>Invoice No. : </strong> <span class="float-end">000028 </span></p>
+                            <p><strong>Data do pedido : </strong> <span class="float-end"> &nbsp;&nbsp;&nbsp;&nbsp; Jan 17, 2016</span></p>
+                            <p><strong>Estado do pedido : </strong> <span class="float-end"><span class="badge bg-danger">Não pago</span></span></p>
+                            <p><strong>Pedido n : </strong> <span class="float-end">0028 </span></p>
                         </div>
                     </div><!-- end col -->
                 </div>
@@ -67,12 +67,12 @@
     
                 <div class="row mt-3">
                     <div class="col-sm-6">
-                        <h6>Billing Address</h6>
+                        <h6>Endereço de cobrança</h6>
                         <address>
                             {{ $customer->address }} - {{ $customer->city }}
                             <br>
-        <abbr title="Phone">Shop Name:</abbr> {{ $customer->shopname }}<br>
-        <abbr title="Phone">Phone:</abbr> {{ $customer->phone }}<br>
+        <abbr title="Phone">Nome da Loja:</abbr> {{ $customer->shopname }}<br>
+        <abbr title="Phone">Celular:</abbr> {{ $customer->phone }}<br>
         <abbr title="Phone">Email:</abbr> {{ $customer->email }}<br>
                         </address>
                     </div> <!-- end col -->
@@ -88,8 +88,8 @@
                 <thead>
                 <tr><th>#</th>
                     <th>Item</th>
-                    <th style="width: 10%">Qty</th>
-                    <th style="width: 10%">Unit Cost</th>
+                    <th style="width: 10%">Qtd</th>
+                    <th style="width: 10%">Preço Unitário</th>
                     <th style="width: 10%" class="text-end">Total</th>
                 </tr></thead>
                 <tbody>
@@ -106,7 +106,7 @@
                     </td>
                     <td>{{ $item->qty }}</td>
                     <td>{{ $item->price }}</td>
-                    <td class="text-end">${{ $item->price*$item->qty }}</td>
+                    <td class="text-end">R${{ $item->price*$item->qty }}</td>
                 </tr>
                 @endforeach
     
@@ -120,7 +120,7 @@
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="clearfix pt-5">
-                            <h6 class="text-muted">Notes:</h6>
+                            <h6 class="text-muted">Observações:</h6>
     
     
                         </div>
@@ -128,8 +128,8 @@
                     <div class="col-sm-6">
                         <div class="float-end">
         <p><b>Sub-total:</b> <span class="float-end">${{ Cart::subtotal() }}</span></p>
-        <p><b>Vat (0%):</b> <span class="float-end"> &nbsp;&nbsp;&nbsp; ${{ Cart::tax() }}</span></p>
-        <h3>${{ Cart::total() }} USD</h3>
+        <p><b>Taxa (0%):</b> <span class="float-end"> &nbsp;&nbsp;&nbsp; R${{ Cart::tax() }}</span></p>
+        <h3>R${{ Cart::total() }} </h3>
                         </div>
                         <div class="clearfix"></div>
                     </div> <!-- end col -->
@@ -138,8 +138,8 @@
     
                 <div class="mt-4 mb-1">
                     <div class="text-end d-print-none">
-                        <a href="javascript:window.print()" class="btn btn-primary waves-effect waves-light"><i class="mdi mdi-printer me-1"></i> Print</a>
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#signup-modal">Create Invoice </button> 
+                        <a href="javascript:window.print()" class="btn btn-primary waves-effect waves-light"><i class="mdi mdi-printer me-1"></i> Imprimir</a>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#signup-modal">Gerar Cobrança </button> 
                     </div>
                 </div>
             </div>
@@ -162,8 +162,8 @@
         <div class="modal-body"> 
             <div class="text-center mt-2 mb-4 ">
                     <div class="auth-logo">
-                        <h3>Invoice Of {{ $customer->name }}</h3>
-                        <h3>Total Amount  ${{ Cart::total() }}</h3>
+                        <h3>Cobrança para {{ $customer->name }}</h3>
+                        <h3>Quantia Total  R${{ Cart::total() }}</h3>
                     </div>
             </div>
 
@@ -174,19 +174,19 @@
                 @csrf
 
                 <div class="mb-3">
-            <label for="username" class="form-label">Payment</label>
+            <label for="username" class="form-label">Pagamento</label>
 <select name="payment_status" class="form-select" id="example-select">
-                <option selected disabled >Select Payment </option>
+                <option selected disabled >Selecionar Pagamento </option>
 
-    <option value="HandCash">HandCash</option>
+    <option value="HandCash">Dinheiro</option>
     <option value="Cheque">Cheque</option>
-    <option value="Due">Due</option>
+    <option value="Due">Gerar Dívida</option>
 
             </select>
                 </div>
 
                     <div class="mb-3">
-            <label for="username" class="form-label">Pay Now</label>
+            <label for="username" class="form-label">Pagar Agora</label>
     <input class="form-control" type="text" name="pay" placeholder="Pay Now">
                 </div>
 
@@ -202,7 +202,7 @@
    <input type="hidden" name="total" value="{{ Cart::total() }}"> 
 
                 <div class="mb-3 text-center">
-    <button class="btn btn-primary" type="submit">Complete Order </button>
+    <button class="btn btn-primary" type="submit">Finalizar Pedido </button>
                 </div>
 
             </form>
